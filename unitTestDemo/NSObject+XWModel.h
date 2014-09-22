c02d3a0c5b8910dc9a20cda793494db71fcbb779
王小天 <wxt1994@gmail.com>
2014-09-22 15:35:08 -0700

    Models created.

---
+++ unitTestDemo/NSObject+XWModel.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+XWModel.h
+//  unitTestDemo
+//
+//  Created by 王小天 on 14-9-22.
+//  Copyright (c) 2014年 Xiaotian Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (XWModel)
+
+@end
---
+++ unitTestDemo/NSObject+XWModel.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+XWModel.m
+//  unitTestDemo
+//
+//  Created by 王小天 on 14-9-22.
+//  Copyright (c) 2014年 Xiaotian Wang. All rights reserved.
+//
+
+#import "NSObject+XWModel.h"
+
+@implementation NSObject (XWModel)
+
+@end
